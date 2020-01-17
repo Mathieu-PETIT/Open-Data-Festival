d8ff8609e421fe7a7fd3d580f271f37af7f995f4
Mathieu-PETIT <57092402+Mathieu-PETIT@users.noreply.github.com>
2020-01-17 17:03:26 +0100

    Create Domaine.py

---
+++ Domaine.py
@@ -0,0 +1,14 @@
+class Domaine():
+    def __init__(self,dom1="",dom2=""):
+        self.dom1=dom1
+        self.dom2=dom2
+
+    def get_dom1(self):
+        return self.dom1
+    def get_dom2(self):
+        return self.dom2
+
+    def set_dom1(self,dom1):
+        self.dom1=dom1
+    def set_dom2(self,dom2):
+        self.dom2=dom2
